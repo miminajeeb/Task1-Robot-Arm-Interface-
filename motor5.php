9fafa4f31fa16a3be46ab504295bd72fb7d7d2b3
Marya Najeeb <38247550+miminajeeb@users.noreply.github.com>
2021-06-18 22:10:19 +0300

    Add files via upload

---
+++ motor5.php
@@ -0,0 +1,23 @@
+<?php
+ error_reporting(0);
+    // connection variables 
+       $host = "localhost";
+       $username = "root";
+       $pwd = "";
+       $db = "robotarm";
+       $port = 3307;
+       
+   // establish connection  
+       $con = mysqli_connect($host, $username, $pwd, $db, $port);
+    
+   // create the query
+        $query = "select motor5 from motorsdegrees ORDER BY id DESC limit 1";
+        
+   // play the query
+        $result = mysqli_query($con, $query);
+        $data = mysqli_fetch_row($result);
+        $degree = $data[0];
+        
+   // print the value on the screen
+        echo $degree;
+?>
